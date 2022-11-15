2fec99ae3adb1db5ef7de364eff2093cb28290ba
James Eapen <james.eapen@vai.org>
2022-11-15 11:26:13 -0500

    fix(telescope): restore my mapping for git_files, use H for help

--- config/nvim/telescope.lua
+++ config/nvim/telescope.lua
@@ -1,9 +1,10 @@
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
+vim.keymap.set('n', '<leader>rg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+vim.keymap.set('n', '<leader>H', builtin.help_tags, {})
 
 
 require("telescope").load_extension("bibtex")
