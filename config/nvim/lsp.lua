871c4bfcdf176ec1f385f18d92071afd67941b3d
James Eapen <james.eapen@vai.org>
2022-12-05 11:36:44 -0500

    fix(nvim:lsp): change mapping for documentation scrolling to meta

--- config/nvim/lsp.lua
+++ config/nvim/lsp.lua
@@ -111,8 +111,8 @@ cmp.setup({
         documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ["<M-u>"] = cmp.mapping.scroll_docs(-4),
+      ["<M-d>"] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
       ['<CR>'] = cmp.mapping.confirm({ select = true }),
