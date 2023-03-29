78ca8a7bfe10e7e34032f8011f74f828a9ec8e2a
James Eapen <james.eapen@vai.org>
2023-03-29 10:39:53 -0400

    fix(nvim:lsp): set completion menu selection to manual

--- config/nvim/lsp.lua
+++ config/nvim/lsp.lua
@@ -114,7 +114,7 @@ cmp.setup({
       ["<M-d>"] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      ['<CR>'] = cmp.mapping.confirm({ select = false }),
     }),
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
