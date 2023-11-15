e5ed925530821d5c369ae8881720e10127c5282c
James Eapen <39574560+jamespeapen@users.noreply.github.com>
2023-11-14 21:48:33 -0500

    feat(nvim:lualine): add os icons

--- config/nvim/lualine.lua
+++ config/nvim/lualine.lua
@@ -1,3 +1,138 @@
+local os_icon = {
+    ["AlmaLinux"] = {
+        icon = " ",
+        color = "#FFFFFF",
+    },
+    ["Alpine Linux"] = {
+        icon = " ",
+        color = "#0d597f",
+    },
+    ["Arch Linux"] = {
+        icon = " ",
+        color = "#1793d1",
+    },
+    ["Artix Linux"] = {
+        icon = " ",
+        color = "#07a1cd",
+    },
+    ["CoreOS"] = {
+        icon = " ",
+        color = "#3c6eb4",
+    },
+    ["Debian GNU/Linux"] = {
+        icon = " ",
+        color = "#dd1155",
+    },
+    ["Deepin"] = {
+        icon = " ",
+        color = "#00b7ff",
+    },
+    ["Devuan"] = {
+        icon = " ",
+        color = "#000000",
+    },
+    ["elementary OS"] = {
+        icon = " ",
+        color = "#3689e6",
+    },
+    ["EndeavourOS"] = {
+        icon = " ",
+        color = "#7f3fbf",
+    },
+    ["Gentoo"] = {
+        icon = " ",
+        color_light = "#54487a",
+        color = "#61538d",
+    },
+    ["Fedora Linux"] = {
+        icon = " ",
+        color = "#3c6eb4",
+        color_light = "#294172",
+    },
+    ["FreeBSD"] = {
+        icon = " ",
+        color = "#ff3300",
+    },
+    ["Kali GNU/Linux"] = {
+        icon = " ",
+        color = "#FFFFFF",
+        color_light = "#0000",
+    },
+    ["Manjaro Linux"] = {
+        icon = " ",
+        color = "#35bf5c",
+    },
+    ["NixOS"] = {
+        icon = " ",
+        color = "#7ebae4",
+        color_light = "#5277c3",
+    },
+    ["OpenBSD"] = {
+        icon = " ",
+        color = "#fef67a",
+        color_light = "#f2ca30",
+    },
+    ["Linux Mint"] = {
+        icon = "󰣭 ",
+        color = "#87cf3e",
+    },
+    ["openSUSE Leap"] = {
+        icon = " ",
+        color = "#73ba25",
+    },
+    ["ParrotOS"] = {
+        icon = " ",
+        color = "#05eeff",
+    },
+    ["Pop!_OS"] = {
+        icon = " ",
+        color = "#6cc7d2",
+    },
+    ["Raspbian GNU/Linux"] = {
+        icon = " ",
+        color = "#bc1142",
+    },
+    ["Red Hat Enterprise Linux"] = {
+        icon = "󱄛 ",
+        color = "#ee0000",
+    },
+    ["Red Hat Enterprise Linux Server"] = {
+        icon = "󱄛 ",
+        color = "#ee0000",
+    },
+    ["Rocky Linux"] = {
+        icon = " ",
+        color = "#10b981",
+    },
+    ["Slackware"] = {
+        icon = " ",
+        color = "#a6b3da",
+        color_light = "#4962ae",
+    },
+    ["Solus"] = {
+        icon = " ",
+        color = "#5294e2",
+        color_light = "#4c5263",
+    },
+    ["Ubuntu"] = {
+        icon =  " ",
+        color = "#e95420",
+    },
+    ["Void Linux"] = {
+        icon =  " ",
+        color = "#abc2ab",
+        color_light = "#478061",
+    },
+    ["ZorinOS"] = {
+        icon =  " ",
+        color = "#15a6f0",
+        color_light = "#478061",
+    },
+}
+
+vim.g.os_icon = os_icon[os.getenv("OS_RELEASE")].icon
+vim.g.os_icon_color = os_icon[os.getenv("OS_RELEASE")].color
+
 require('lualine').setup {
   options = {
     icons_enabled = true,
@@ -21,7 +156,11 @@ require('lualine').setup {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
     lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_x = {
+        'encoding',
+        { 'g:os_icon', color = {fg = vim.g.os_icon_color} },
+        {'filetype', icon_only = true},
+    },
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
