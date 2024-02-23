d848000acf102ab2c2e21b4514ed4689908fa9c6
James Eapen <james.eapen@vai.org>
2024-02-23 15:27:52 -0500

    fix(nvim:lsp): remove redefinition of cmp

--- config/nvim/lsp.lua
+++ config/nvim/lsp.lua
@@ -82,7 +82,7 @@ require("nvim-autopairs").setup {}
   require('nvim-autopairs').remove_rule('`')
 -- If you want insert `(` after select function or method item
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local cmp = require('cmp')
+
 cmp.event:on(
   'confirm_done',
   cmp_autopairs.on_confirm_done()
