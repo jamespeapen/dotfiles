7688758200fe3c68ce8ff5bb4bee9dd5695a8ac8
James Eapen <james.eapen@vai.org>
2026-02-25 15:39:24 -0500

    fix(treesitter): update grammar install method
    switch to treesitter-modules for deprecated functions

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -3,62 +3,62 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function ()
-      local configs = require("nvim-treesitter")
-      configs.setup({
-        install = {
-          "cpp",
-          "lua",
-          "markdown",
-          "markdown_inline",
-          "python",
-          "r",
-          "rnoweb",
-          "rust",
-          "vim",
-          "vimdoc",
-          "yaml",
-        },
-        ignore_install = {
-          "javascript"
-        },
-        highlight = {
-          enable = true,
-          -- disable = { "markdown" },
-        },
-        sync_install = false,
-        indent = {
-          enable = true
-        },
-        incremental_selection = {
-          enable = true,              -- false will disable the whole extension
-          additional_vim_regex_highlighting = false,
-          keymaps = {
-            init_selection = "<C-s>",
-            node_incremental = "<a-n>",
-            scope_incremental = "<a-c>",
-            node_decremental = "<a-p>",
-          },
-        },
-        refactor = {
-          highlight_definitions = { enable = true },
-          navigation = {
-            enable = true,
-            keymaps = {
-              -- list_definitions = "gnd",
-              list_definitions_toc = "g0",
-              goto_next_usage = "g]",
-              goto_previous_usage = "g[",
-            }
-          },
-          smart_rename = {
-            enable = true,
-            keymaps = {
-              smart_rename = "grr",
-            },
-          }
-        }
+      local ts = require("nvim-treesitter")
+      langs = {
+        "cpp",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "r",
+        "rnoweb",
+        "rust",
+        "vim",
+        "vimdoc",
+        "yaml",
+      }
+      ts.install(langs)
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = langs,
+        callback = function() vim.treesitter.start() end,
       })
     end
+  },
+  {
+    'MeanderingProgrammer/treesitter-modules.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    opts = {
+      indent = {
+        enable = true
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-s>",
+          node_incremental = "<a-n>",
+          scope_incremental = "<a-c>",
+          node_decremental = "<a-p>",
+        },
+      },
+      refactor = {
+        highlight_definitions = { enable = true },
+        navigation = {
+          enable = true,
+          keymaps = {
+            list_definitions = "gnd",
+            list_definitions_toc = "g0",
+            goto_next_usage = "g]",
+            goto_previous_usage = "g[",
+          }
+        },
+        smart_rename = {
+          enable = true,
+          keymaps = {
+            smart_rename = "grr",
+          },
+        },
+      },
+    },
   },
   {
     "nvim-treesitter/nvim-treesitter-context",
