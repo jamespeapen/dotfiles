a40c3bfd5e9e74cbd6ceb89e16e3b48921d94a2b
James Eapen <james.eapen@vai.org>
2026-04-23 13:42:49 -0400

    fix(nvim:lspconfig): add virtual text to diagnostics
    https://influentcoder.com/posts/nvim-diagnostics/

--- config/nvim/lua/lspconfig.lua
+++ config/nvim/lua/lspconfig.lua
@@ -26,6 +26,17 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end,
 })
 
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = "●",
+    spacing = 2,
+  },
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
+
 -- -- Bash
 vim.lsp.enable('bashls')
 
