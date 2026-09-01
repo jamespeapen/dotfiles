0d20dbe8a82000b601a76468592ed1ddf32f2a50
James Eapen <james.eapen@vai.org>
2026-09-01 11:27:10 -0400

    fix(my_funcs): stopwatch display every second

--- config/zsh/my_funcs.zsh
+++ config/zsh/my_funcs.zsh
@@ -127,7 +127,7 @@ function duck() {
 function stopwatch() {
   date1=`date +%s`; while true; do 
    echo -ne "$(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)\r";
-   sleep 5
+   sleep 1
   done
 }
 
